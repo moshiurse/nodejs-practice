9663e3c9a506901313c24f1ca62140f17d489712
Moshiur Rahman <rmoshiur705@gmail.com>
2020-03-17 23:24:17 +0600

    send an html reponse from server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Nodejs Practice</title></head>");
+  res.write("<body><h1>Hello from nodejs Response</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
