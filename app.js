f7949205878381c999e6ab18b4c187678f13db9f
Moshiur Rahman <rmoshiur705@gmail.com>
2020-03-26 07:06:34 +0600

    Used node modules & seperate route files

--- app.js
+++ app.js
@@ -1,46 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Nodejs Practice</title></head>");
-    res.write(
-      "<body><form action='/msg' method='POST'><input type='text' name='msg'/><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/msg" && method === "POST") {
-    const body = [];
-    req.on("data", chunk => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      console.log(parseBody);
-      const msg = parseBody.split("=")[1];
-      fs.writeFile("message.txt", msg, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.write("<html>");
-  //   res.write("<head><title>Message</title></head>");
-  //   res.write("<body><h1>Hello from nodejs Response</h1></body>");
-  //   res.write("</html>");
-  //   res.end();
-});
+const server = http.createServer(routes); //for way2 & 3 & 4 use routes.handler in parameter
 
 server.listen(3000);
