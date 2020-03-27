24717836dece8b096e37c7415893e488171ebf78
Moshiur Rahman <rmoshiur705@gmail.com>
2020-03-27 10:46:13 +0600

    using express router

--- app.js
+++ app.js
@@ -3,25 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // use bodyParser
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 // use middleware
-app.use('/', (req, res, next) => {
-    console.log("Middleware runs always!!");
-    next();
-})
+// app.use('/', (req, res, next) => {
+//     console.log("Middleware runs always!!");
+//     next();
+// })
 
-app.use('/add',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="pname"><button type="submit">Submit</button></form>');
-})
-
-app.use('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-    res.send('<h3>Hello From Moshiur Rahman</h3>');
-})
 
 app.listen(3000);
