5fbbafcd91ed8baf44a397831a61664bb7b8506d
Moshiur Rahman <rmoshiur705@gmail.com>
2020-03-27 11:33:52 +0600

    created html poages and served && using path helper to get rootDirectory

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/add',(req, res, next) => {
-    res.send('<form action="/admin/add" method="POST"><input type="text" name="pname"><button type="submit">Submit</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add',(req, res, next) => {
