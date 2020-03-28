a4b24bf2a3ffcd0dd6f59a5441d6d650e2d1a9ce
Moshiur Rahman <rmoshiur705@gmail.com>
2020-03-28 21:17:28 +0600

    pug integrated and views created & added products in array

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@ const router = express.Router();
 
 const rootDir = require('../util/path');
 
+const products = [];
+
 router.get('/add',(req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add',(req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,12 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop');
 })
 
 module.exports = router;
