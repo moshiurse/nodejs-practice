6a1ce0f1cfb0a9d10b495a4621188625bb3c61dd
Moshiur Rahman <rmoshiur705@gmail.com>
2020-04-01 23:42:52 +0600

    create Controllers and integrated all Pages

--- routes/admin.js
+++ routes/admin.js
@@ -4,18 +4,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products');
 
-const products = [];
+router.get('/add', productController.getAddProduct);
 
-router.get('/add',(req, res, next) => {
-    res.render('add-product', {title: 'Add Product', path: '/admin/add', products: true, activeAddProduct: true, productCss: true});
-})
+router.post('/add', productController.postAddProduct);
 
-router.post('/add',(req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-})
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
