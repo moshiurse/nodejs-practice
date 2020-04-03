43889e5d8d5b1ce4b114c777623c4a4e1a5026d5
Moshiur Rahman <rmoshiur705@gmail.com>
2020-04-04 01:45:24 +0600

    added more product data synamically & added order component

--- routes/shop.js
+++ routes/shop.js
@@ -12,6 +12,8 @@ router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
+router.get('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
