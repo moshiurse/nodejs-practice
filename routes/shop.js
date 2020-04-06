2b7640337cd5907465c6099df354d060d22de1f3
Moshiur Rahman <rmoshiur705@gmail.com>
2020-04-06 07:10:14 +0600

    Add Product to Cart JSON

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get('/products/:pId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
