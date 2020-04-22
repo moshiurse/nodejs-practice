eff0a335e613c84c461e78b18a1364dfc22632a9
Moshiur Rahman <rmoshiur705@gmail.com>
2020-04-22 21:37:14 +0600

    Integrated Mongodb & setup

--- util/database.js
+++ util/database.js
@@ -1,9 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('nodejs-practice', 'root', '', 
-{
-    dialect: 'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://root:12345@moshiurscluster-mkbxp.mongodb.net/test?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+    if(_db){
+        return _db;
+    }else{
+        throw "No Database found";
+    }
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
