b40ebf7d3f9d985e84d77436cdf6331138048b3b
Moshiur Rahman <rmoshiur705@gmail.com>
2020-05-04 22:47:54 +0600

    Displaying & deleting cart Items using mongodb

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,17 +43,13 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
     req.user.getCart()
-    .then(cart => {
-        return cart.getProducts()
-            .then(products => {
-                res.render('shop/cart', 
-                {
-                    path: '/cart',
-                    title: 'Cart',
-                    products: products
-                })
-                .catch(err => console.log(err));
-            })
+    .then(products => {
+        res.render('shop/cart', 
+        {
+            path: '/cart',
+            title: 'Cart',
+            products: products 
+        })
     })
     .catch(err => console.log(err));
     
@@ -63,55 +59,19 @@ exports.postCart = (req, res, next) => {
     const productId = req.body.productId;
     Product.findById(productId)
     .then(product => {
-        req.user.addToCart(product);
+        return req.user.addToCart(product);
     })
     .then(result => {
+        res.redirect('/cart');
         console.log(result);
     })
-    // let fetchedCart;
-    // req.user
-    //     .getCart()
-    //     .then(cart => {
-    //         fetchedCart = cart;
-    //         let newQty = 1;
-    //         return cart.getProducts({where: {id: productId}})
-    //         .then(products => {
-    //             let product;
-    //             if(products.length > 0 ){
-    //                 product = products[0];
-    //             }
-
-    //             if(product){
-    //                 const oldQty = product.cartItem.quantity;
-    //                 newQty = oldQty +1;
-    //                 return product;
-    //             }
-    //             return Product.findByPk(productId)
-    //         })
-    //         .then(product => {
-    //             return fetchedCart.addProduct(product, 
-    //                 { through: { quantity: newQty }
-    //             });
-    //         })
-    //         .then(() => {
-    //             res.redirect('/cart');
-    //         })
-    //             .catch(err => console.log(err));
-    //         })
-            
+   
 };
 
 exports.postDeleteCart = (req, res, next) => {
     const productId = req.body.productId;
     
-    req.user.getCart()
-    .then(cart => {
-        return cart.getProducts({where: {id: productId}})
-    })
-    .then(products => {
-        const product = products[0];
-        return product.cartItem.destroy();
-    })
+    req.user.deleteItemFromCart(productId)
     .then(result => {
         res.redirect('/cart');
     })
