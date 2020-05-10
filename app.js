d52bf91123a4e2124ddc750eb8972d9cd67fd84b
Moshiur Rahman <rmoshiur705@gmail.com>
2020-05-11 00:16:51 +0600

    Modifying styles and designs of some page && added login page

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // app.use(errorController.get404);
 
