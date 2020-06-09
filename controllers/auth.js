7c6d615dd1744c73b85be9e4f0091ab4411661db
Moshiur Rahman <rmoshiur705@gmail.com>
2020-06-10 00:08:03 +0600

    added sending mail to signed up users feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-
+const nodemailer = require('nodemailer');
 const User =  require('../model/user');
 
 exports.getLogin = (req, res, next) => {
@@ -99,7 +99,51 @@ exports.postSignup = (req, res, next) => {
         })
 
     .then(result => {
+
+// nodemailer sending mail
+        const output = `
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>  
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+        </ul>
+        <p>User created successfully</p>
+        `;
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'mail.moshiurse.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: 'mailtest@moshiurse.com',
+          pass: 'tUzEf4zo+dgj'  // generated ethereal password
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+  
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Moshiur Rahman" <mailtest@moshiurse.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Testing my mail server in nodejs', // Subject line
+        text: 'Hello NodeJs Dev', // plain text body
+        html: output // html body
+    };
+  
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
         res.redirect('/login')
+    });
     })
 })
     .catch(err => console.log(err));
